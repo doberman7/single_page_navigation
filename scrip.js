b0190f3aea9ff582784e396a1865155da949e104
doberman7 <ivanrubioangeles@gmail.com>
2017-07-04 23:26:10 -0500

    Uso de  ^ para apuntar a cualquier href y hacer smoothScroll

--- scrip.js
+++ scrip.js
@@ -11,9 +11,21 @@ $(document).ready(function(){
         ^  does the trick
       */
       $("a[href^='#']")
-        .unbind('click.smoothScroll')
+        /*
+          unbind: Remove a previously-attached event handler from the elements DEPRACTED since 3.0
+          comentado por que hacia venia en el ejercio pero no parece hacer algo
+        */
+        //.unbind('click.smoothScroll')
+
+
+        // Attach a handler to an event for the elements.
         .bind('click.smoothScroll', function(event) {
-          event.preventDefault();
+
+          //comentado por que hacia venia en el ejercio pero no parece hacer algo
+          //event.preventDefault();
+
+          //scrollTop: Get the current vertical position of the scroll bar for the first element in the set of matched elements or set the vertical position of the scroll bar for every matched element.
+          //offset: Get the current coordinates of the first element, or set the coordinates of every element, in the set of matched elements, relative to the document.
           $('html, body').animate({ scrollTop: $( $(this).attr('href') ).offset().top }, speed);
         });
     });
